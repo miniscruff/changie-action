77224741ba9e8d4219d277b9097e014141a4f694
Ronnie Smith <halfpint1170@gmail.com>
2022-10-22 18:42:24 -0700

    small log changes and a new build

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as exec from "@actions/exec";
 async function run(): Promise<void> {
   try {
     const inputs: context.Inputs = await context.getInputs();
-    core.info(`Changie inputs ${inputs}`);
+    core.debug(`Changie inputs ${inputs}`);
 
     const bin = await changie.install(inputs.version);
     core.info(`Changie ${inputs.version} installed successfully`);
@@ -16,6 +16,7 @@ async function run(): Promise<void> {
     core.addPath(changieDir);
     core.debug(`Added ${changieDir} to PATH`);
 
+    core.debug(`Running changie: '${inputs.args}'`);
     await exec.exec(`${bin} ${inputs.args}`, undefined, {});
   } catch (error) {
     core.setFailed(error.message);
