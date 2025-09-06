9b20dca5e91979c0cf8e51a7145fe03ca0cd16db
Ronnie Smith <miniscruff@hey.com>
2025-09-06 16:41:49 -0700

    fix args typo and add fragments

--- src/context.ts
+++ src/context.ts
@@ -14,6 +14,6 @@ export async function getInputs(): Promise<Inputs> {
   return {
     version: core.getInput("version") || "latest",
     args: core.getInput("args"),
-    workdir: core.getInput("args") || '.',
+    workdir: core.getInput("workdir") || '.',
   };
 }
